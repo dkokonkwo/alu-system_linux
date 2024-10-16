ea6c7730857dea873979ffc7e1c2a1d745bbab48
dkokonkwo <d.okonkwo@alustudent.com>
2024-10-16 13:31:59 -0300

    new hnm.c

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - replicates the functionality of nm -p
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success else 1
- */
-int main(int argc, char *argv[])
-{
-if (argc != 2)
-{
-fprintf(stderr, "Usage: %s [objfile]\n", argv[0]);
-return (1);
-}
-char command[256];
-snprintf(command, sizeof(command), "nm -p %s", argv[1]);
-int result = system(command);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s [object_file]\n", argv[0]);
+        return 1;
+    }
 
-if (result != 0)
-{
-fprintf(stderr, "Error: failed to execute command.\n");
-return (1);
-}
-return (0);
+    char command[256];
+    snprintf(command, sizeof(command), "nm -p %s 2>&1", argv[1]);
+    FILE *fp = popen(command, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error: failed to execute nm command.\n");
+        return 1;
+    }
+
+    char line[256];
+    int found_symbols = 0;
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (strstr(line, "no symbols") != NULL) {
+            fprintf(stderr, "%s: %s", argv[0], line);
+            found_symbols = 1;
+        } else {
+            printf("%s", line);
+        }
+    }
+
+    pclose(fp);
+    return found_symbols ? 1 : 0;
 }
