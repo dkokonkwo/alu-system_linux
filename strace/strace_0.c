807b96754a3bbed2b275b9a774836be23c01db04
dkokonkwo <d.okonkwo@alustudent.com>
2024-10-30 16:05:15 -0300

    full

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -13,7 +13,7 @@
  * @argv: array of arguments
  * Return: 0 sucess, 1 on failure
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])
 {
 int print, status;
 struct user_regs_struct regs;
@@ -25,6 +25,7 @@ fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
 return (1);
 }
 
+setbuf(stdout, NULL);
 child = fork();
 if (child == -1)
 {
@@ -33,18 +34,20 @@ return (1);
 }
 else if (child == 0)
 {
-ptrace(PTRACE_TRACEME, 0, 0, 0);
-argc[argc] = 0;
-execvp(argv[1], argv+1);
+printf("59\n");
+ptrace(PTRACE_TRACEME, child, NULL, NULL);
+execve(argv[1], argv + 1, envp);
 }
-wait(&status);
-ptrace(PTRACE_SYSCALL, child, 0, 0);
-wait(&status);
-ptrace(PTRACE_GETREGS, child, 0, &regs);
-print = regs.orig_eax;
-if (print == __NR_open)
+else
 {
-printf("%d\n", print);
+for (status = 1, print = 0; !WIFEXITED(status); print ^= 1)
+{
+ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+wait(&status);
+ptrace(PTRACE_GETREGS, child, NULL, &regs);
+if (print)
+printf("%lu\n", (size_t)regs.orig_rax);
 }
-return 0;
+}
+return (0);
 }
