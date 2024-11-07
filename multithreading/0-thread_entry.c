2a98c71bafa5088e8d191ec1c3cae363b316111c
dkokonkwo <d.okonkwo@alustudent.com>
2024-11-07 12:07:55 -0400

    betty

--- multithreading/0-thread_entry.c
+++ multithreading/0-thread_entry.c
@@ -3,9 +3,10 @@
 /**
  * thread_entry - entry point to a new thread
  * @arg: address of string to be printed
+ * Return: Nothing
  */
 void *thread_entry(void *arg)
 {
-    printf("%s\n", (char *)arg);
-    pthread_exit(NULL);
+printf("%s\n", (char *)arg);
+pthread_exit(NULL);
 }
