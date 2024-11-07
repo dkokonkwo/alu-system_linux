a3e1131918364c99562ca30f2e2decc9f7634601
dkokonkwo <d.okonkwo@alustudent.com>
2024-11-07 14:02:37 -0400

    next

--- multithreading/10-blur_portion.c
+++ multithreading/10-blur_portion.c
@@ -12,7 +12,7 @@ pixel_t *apply_kernel(img_t *img, kernel_t *kernel, size_t px, size_t py)
 {
 int half = kernel->size / 2;
 float sum_r = 0, sum_g = 0, sum_b = 0, sum = 0;
-int kx, ky
+int kx, ky;
 size_t nx, ny;
 size_t pixel_idx;
 float kernel_value;
