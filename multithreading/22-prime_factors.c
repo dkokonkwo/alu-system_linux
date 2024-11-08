636ab506fc202baf3064789a44107343ad6ecb3b
dkokonkwo <d.okonkwo@alustudent.com>
2024-11-08 12:25:50 -0400

    moved functions

--- multithreading/22-prime_factors.c
+++ multithreading/22-prime_factors.c
@@ -86,3 +86,45 @@ tprintf("[%02d] Failure\n", tid);
 }
 return (NULL);
 }
+
+/**
+ * exec_task - executes task in node
+ * @task: pointer to task to execute
+ * Return: result
+ */
+void *exec_task(task_t *task)
+{
+void *result;
+result = task->entry(task->param);
+pthread_mutex_lock(&task->lock);
+task->result = result;
+pthread_mutex_unlock(&task->lock);
+return (result);
+}
+
+/**
+ * task_stat - gets task's status
+ * @task: pointer to task
+ * Return: task status
+ */
+task_status_t task_stat(task_t *task)
+{
+    task_status_t stat;
+    pthread_mutex_lock(&task->lock);
+    stat = task->status;
+    pthread_mutex_unlock(&task->lock);
+    return (stat);
+}
+
+/**
+ * set_task_stat - sets task's status
+ * @task: pointer to task
+ * @stat: new status to set
+ * Return: Nothing
+ */
+void set_task_stat(task_t *task, task_status_t stat)
+{
+    pthread_mutex_lock(&task->lock);
+    task->status = stat;
+    pthread_mutex_unlock(&task->lock);
+}
