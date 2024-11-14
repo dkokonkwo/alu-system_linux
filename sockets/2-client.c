a66790132c312c2f3a24ea44c23aa288d0b1e6c2
dkokonkwo <d.okonkwo@alustudent.com>
2024-11-14 15:50:38 +0100

    less lines

--- sockets/2-client.c
+++ sockets/2-client.c
@@ -19,44 +19,35 @@ struct addrinfo hints, *res, *p;
 if (argc < 3)
 {
 printf("Usage: %s <host> <port>\n", argv[0]);
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 sockfd = socket(AF_INET, SOCK_STREAM, 0);
 if (sockfd < 0)
 {
 perror("Socket creation error");
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 memset(&hints, 0, sizeof(hints));
 hints.ai_family = AF_INET;
 hints.ai_socktype = SOCK_STREAM;
-
 if (getaddrinfo(argv[1], argv[2], &hints, &res) != 0)
 {
 perror("getaddrinfo");
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 for (p = res; p != NULL; p = p->ai_next)
 {
 sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
 if (sockfd < 0)
 {
 perror("Socket creation error");
-continue;
-}
+continue; }
 if (connect(sockfd, p->ai_addr, p->ai_addrlen) == 0)
-{
 break;
-}
 perror("Connection failed");
-close(sockfd);
-}
+close(sockfd); }
 if (p == NULL)
 {
 fprintf(stderr, "Failed to connect\n");
 freeaddrinfo(res);
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 printf("Connected to %s:%s\n", argv[1], argv[2]);
 freeaddrinfo(res);
 close(sockfd);
