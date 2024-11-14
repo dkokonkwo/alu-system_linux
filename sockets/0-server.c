c7fdd413409c3f1ccfa4ec76d00ab9c19dd0ff47
dkokonkwo <d.okonkwo@alustudent.com>
2024-11-14 14:20:22 +0100

    0-server.c

---
+++ sockets/0-server.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arp/inet.h>
+#include <sys/socket.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+/**
+ * main - opens IPV4/TCP socket and listens on a port
+ * Return: 0 always
+ */
+int main()
+{
+int server_fd, new_socket;
+struct sockaddr_in address;
+int addrlen = sizeof(address);
+
+server_fd = socket(AF_INET, SOCK_STREAM, 0);
+if (server_fd == -1)
+{
+perror("Socket failed");
+exit(EXIT_FAILURE);
+}
+
+address.sin_family = AF_INET;
+address.sin_addr.s_addr = INADDR_ANY;
+address.sin_port = htond(PORT);
+
+if (bind(server_fd, (struct sockaddr *)&address, addrlen) < 0)
+{
+perror("Bind failed");
+close(server_fd);
+exit(EXIT_FAILURE);
+}
+
+if (listen(server_fd, 3) < 0)
+{
+perror("Listen failed");
+close(server_fd);
+exit(EXIT_FAILURE);
+}
+printf("Server listening on port %s\n", PORT);
+
+
+close(new_socket);
+close(server_fd);
+return(0);
+}
