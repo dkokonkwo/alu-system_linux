25d5fe67d6ac44bdf0e31b977aa16221b7111e48
dkokonkwo <d.okonkwo@alustudent.com>
2024-11-14 16:07:18 +0100

    fewer lines in function

--- sockets/3-server.c
+++ sockets/3-server.c
@@ -22,8 +22,7 @@ server_fd = socket(AF_INET, SOCK_STREAM, 0);
 if (server_fd == -1)
 {
 perror("Socket failed");
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 address.sin_family = AF_INET;
 address.sin_addr.s_addr = INADDR_ANY;
 address.sin_port = htons(PORT);
@@ -31,14 +30,12 @@ if (bind(server_fd, (struct sockaddr *)&address, addrlen) < 0)
 {
 perror("Bind failed");
 close(server_fd);
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 if (listen(server_fd, 3) < 0)
 {
 perror("Listen failed");
 close(server_fd);
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 printf("Server listening on port %d\n", PORT);
 new_socket = accept(server_fd, (struct sockaddr *)&address,
 (socklen_t *)&addrlen);
@@ -46,18 +43,14 @@ if (new_socket < 0)
 {
 perror("Accept failed");
 close(server_fd);
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 printf("Client connected: %s\n", inet_ntoa(address.sin_addr));
 while (1)
 {
 memset(buffer, 0, BUFFER_SIZE);
 valread = recv(new_socket, buffer, BUFFER_SIZE, 0);
 if (valread <= 0)
-{
-printf("Client disconnected.\n");
 break;
-}
 printf("Message received: \"%s\"\n", buffer);
 break;
 }
