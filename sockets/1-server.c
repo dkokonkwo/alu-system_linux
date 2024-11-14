2a9939316aa35744706ca9d27c6d560676f35e71
dkokonkwo <d.okonkwo@alustudent.com>
2024-11-14 14:46:01 +0100

    betty style corrections

--- sockets/1-server.c
+++ sockets/1-server.c
@@ -16,14 +16,12 @@ int main(void)
 int server_fd, new_socket;
 struct sockaddr_in address;
 int addrlen = sizeof(address);
-
 server_fd = socket(AF_INET, SOCK_STREAM, 0);
 if (server_fd == -1)
 {
 perror("Socket failed");
 exit(EXIT_FAILURE);
 }
-
 address.sin_family = AF_INET;
 address.sin_addr.s_addr = INADDR_ANY;
 address.sin_port = htons(PORT);
@@ -34,7 +32,6 @@ perror("Bind failed");
 close(server_fd);
 exit(EXIT_FAILURE);
 }
-
 if (listen(server_fd, 3) < 0)
 {
 perror("Listen failed");
@@ -43,7 +40,8 @@ exit(EXIT_FAILURE);
 }
 printf("Server listening on port %d\n", PORT);
 
-new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+new_socket = accept(server_fd, (struct sockaddr*)&address,
+(socklen_t*)&addrlen);
 if (new_socket < 0)
 {
 perror("Accept failed");
@@ -51,7 +49,6 @@ close(server_fd);
 exit(EXIT_FAILURE);
 }
 printf("Client connected: %s\n", inet_ntoa(address.sin_addr));
-
 close(new_socket);
 close(server_fd);
 return (0);
